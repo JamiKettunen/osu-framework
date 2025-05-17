010f5e95f28f2e9ae010e440cf6a68934e29b6e9
Jami Kettunen <jami.kettunen@protonmail.com>
2025-05-17 14:08:28 +0300

    Revert "Remove Linux hwaccel features from FFmpeg build"
    This reverts commit 98da3979626a9fd8ba211852feeec7cd08b7592f.

--- osu.Framework.NativeLibs/scripts/ffmpeg/build-linux.sh
+++ osu.Framework.NativeLibs/scripts/ffmpeg/build-linux.sh
@@ -32,6 +32,13 @@ case $arch in
 esac
 
 FFMPEG_FLAGS+=(
+    # --enable-vaapi
+    # --enable-vdpau
+    # --enable-hwaccel='h264_vaapi,h264_vdpau'
+    # --enable-hwaccel='hevc_vaapi,hevc_vdpau'
+    # --enable-hwaccel='vp8_vaapi,vp8_vdpau'
+    # --enable-hwaccel='vp9_vaapi,vp9_vdpau'
+
     --target-os=linux
 )
 
